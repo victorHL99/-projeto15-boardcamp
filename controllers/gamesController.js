8c94225aba8216c9954af2a6cc2a5442cb666218
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-29 20:14:50 -0300

    feat: rota para post games funcionando

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -12,3 +12,16 @@ export async function getGames(req,res) {
     }
 
 };
+
+export async function postGames(req,res) {
+    console.log("Passando pela controller post games");
+    const {name,image, stockTotal ,categoryId,pricePerDay} = req.body;
+    console.log(req.body);
+    try{
+        const resultGames = await db.query('INSERT INTO games (name, "image", "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)', [name, image, stockTotal, categoryId, pricePerDay]);
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+};
--- middlewares/verifiersMiddlewares.js
+++ middlewares/verifiersMiddlewares.js
@@ -29,6 +29,36 @@ export async function vPostCategoriesMid(req, res, next){
         console.log(error);
         res.sendStatus(500);
     }
-
-
 };
+
+export async function vPostGamesMid(req,res,next){
+    console.log("Passando pela middleware post games")
+    const {name,image,stockTotal,categoryId,pricePerDay} = req.body;
+
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        image: Joi.string().required().pattern(new RegExp(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/)).required(),
+        stockTotal: Joi.number().required().greater(0).integer(),
+        categoryId: Joi.number().required().integer().min(1).max(3),
+        pricePerDay: Joi.number().required().greater(0).integer()
+    });
+
+    const verifySchema = schema.validate({name, image, stockTotal, categoryId, pricePerDay}).error;
+    if(verifySchema){
+        res.sendStatus(400);
+        return;
+    }
+
+    try{
+        const resultGames = await db.query('SELECT * FROM games WHERE name = $1',[name]);
+        if(resultGames.rows.length > 0){
+            res.sendStatus(409);
+            return;
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
