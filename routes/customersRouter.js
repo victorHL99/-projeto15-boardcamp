32bd67c709ff7796749c825737ce3a8464e89801
victorHL99 <victor.lessa@estudante.ufla.br>
2022-05-30 13:32:57 -0300

    feat: rota de get para customer por id criada

--- routes/customersRouter.js
+++ routes/customersRouter.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 
-import {getCustomers} from '../controllers/customersController.js';
+import {getCustomers, getCustomerId} from '../controllers/customersController.js';
 
 const customersRouter = Router();
 console.log("Passando pela rota customers");
 
 customersRouter.get("/customers", getCustomers);
+customersRouter.get("/customers/:id", getCustomerId);
 
 export default customersRouter;
